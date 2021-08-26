648f19723e63e176131d07232ad99489f9ebebdd
Amirsil <57900754+Amirsil@users.noreply.github.com>
2021-08-26 19:07:11 +0300

    Create generate-cert.sh

---
+++ generate-cert.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+HOSTNAME=$1
+
+if [ -z $HOSTNAME ]
+then
+    echo Usage: ./gencrt [HOSTNAME]
+    exit
+else
+    echo Generating Certificate...
+fi
+
+openssl req -new -nodes -newkey rsa:4096 \
+  -keyout $HOSTNAME.key \
+  -out $HOSTNAME.csr \
+  -subj "/C=US/ST=State/L=City/O=Some-Organization-Name/CN=$HOSTNAME" &>/dev/null
+
+cat > v3.ext <<-EOF
+authorityKeyIdentifier=keyid,issuer
+basicConstraints=CA:FALSE
+keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment
+subjectAltName = @alt_names
+[alt_names]
+IP.1 = 192.168.77.252
+IP.2 = $(host $HOSTNAME | awk '{ print $4 }')
+IP.3 = 127.0.0.1
+DNS.1 = localhost
+DNS.2 = $HOSTNAME
+DNS.3 = $HOSTNAME.unixmen.local
+DNS.6 = app
+DNS.7 = matan
+DNS.8 = matan.unixmen.local
+DNS.9 = ori
+DNS.10 = ori.unixmen.local
+EOF
+
+openssl x509 -req -sha512 -days 365 \
+  -extfile v3.ext \
+  -CA root.crt -CAkey root.key -CAcreateserial \
+  -in $HOSTNAME.csr \
+  -out $HOSTNAME.crt &>/dev/null
+
+echo Sending Certificate to target...
+scp $HOSTNAME.crt $HOSTNAME:$HOSTNAME.crt > /dev/null
+scp $HOSTNAME.key $HOSTNAME:$HOSTNAME.key > /dev/null
+rm $HOSTNAME.crt
+rm $HOSTNAME.key
+rm $HOSTNAME.csr
+rm v3.ext
+
+echo Done!
